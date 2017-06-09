16c9da5781213b3a3d948d24a9da86eec2e8616c
taoyuyu <taoyu@whu.edu.cn>
2017-06-09 13:31:06 +0800

    test code

--- src/test/java/TestHDFS.java
+++ src/test/java/TestHDFS.java
@@ -1,7 +1,11 @@
-package PACKAGE_NAME;
+import java.io.IOException;
 
 /**
  * Created by yutao on 17/6/9.
  */
 public class TestHDFS {
+    public static void main(String[] args) throws IOException {
+        ReadFile rf = new ReadFile();
+        rf.readFile("hdfs://localhost:9000/user/taoyu/test");
+    }
 }
